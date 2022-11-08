3b09208b0ebd40002805b5ef8ca76f22cbe7c751
EmilMurahas228 <murakhas@list.ru>
2022-11-08 18:21:10 +0300

    feat: complete project

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGihubUser } from "../types";
+
+export const defaultUser: LocalGihubUser = {
+  login: "EmilMurahas228",
+  avatar: "https://avatars.githubusercontent.com/u/60827113?v=4",
+  name: "Emil",
+  company: "",
+  blog: "portfolio-website-alpha-wine.vercel.app/",
+  location: "Crimea",
+  bio: "React Frontend Developer",
+  twitter: "",
+  repos: 32,
+  followers: 7,
+  following: 6,
+  created: "2020-02-08T20:12:13Z",
+};
